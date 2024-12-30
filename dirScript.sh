67be5b607f0f2875bedd5227ef92078bd8b013e8
2091a <anshulbhattglt@gmail.com>
2024-12-30 12:43:46 +0000

    script fixed

--- dirScript.sh
+++ dirScript.sh
@@ -4,13 +4,14 @@ echo "lets create new directory Give directory name "
 read dirName
 mkdir $dirName
 echo "navigateing to new directory "
-cd $dirName
+cd "$dirName"
 echo "lets create a file inside new directory"
 echo "Give file name "
 read newFile
-touch $newFile
+touch "$newFile"
 echo "file created lets see the content of the directory"
-ls -lt $dirName
+ls -lt 
+cd ..
 echo "do you want to delete the directory"
 rm -ri $dirName
 
