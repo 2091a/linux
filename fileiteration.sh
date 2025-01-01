1f1d74f13ae2eaaee013fe20f773e4cb0fb4014c
2091a <anshulbhattglt@gmail.com>
2025-01-01 12:52:49 +0000

    final fix

--- fileiteration.sh
+++ fileiteration.sh
@@ -8,12 +8,12 @@ dir_count=0
 
 for item in "$dir_path"/*; do
 	if [ -f "$item" ] ; then
-		echo "file: $(basename "$item") size: $(stat -c%s "$item") byts"
-		((file_count++))
+		echo "file: $(basename "$item") size: $(stat -c%s "$item") bytes"
+		file_count=$((file_count + 1))
 	elif [ -d "$item" ];
 	then
 		echo "directory: $(basename "$item")"
-		((dir_count++))
+		dir_count=$((dir_count + 1))
 	fi
 done
 	echo "Total files: $file_count & Total dirctory: $dir_count"
