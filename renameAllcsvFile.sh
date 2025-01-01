216e38c684947b22cbb43715e7c902edd48a2333
2091a <anshulbhattglt@gmail.com>
2025-01-01 13:35:42 +0000

    unfix

--- renameAllcsvFile.sh
+++ renameAllcsvFile.sh
@@ -1,9 +1,15 @@
 dir="/home/vboxuser"
 
-for file in "$dir"/*.csv ;do
+for file in "$dir"/*.csv; do
 	echo "$(basename $file) give new name for it"
-	newname= read newname
-	 mv $file $newname".csv"
+	 read newname
+	 if [ -d "$dir/$newname.csv" ];then
+		 echo "file already exists"
+		 continue
+	 fi
+
+	 newfile= "$dir/$newname.csv"
+	 mv "$file" "$newfile"
 	 [ $? -eq 0 ] && echo "name changed sucessfuly --"
  done
 
