e547db5bd10b69d8cdecd0f66cdd16ef543b172e
2091a <anshulbhattglt@gmail.com>
2025-01-03 14:51:03 +0000

    some fix

--- upercaseConverter.bash
+++ upercaseConverter.bash
@@ -10,7 +10,7 @@ while read -r line; do
 	echo "converting file to uper case"
 
 	 upercase= "$line" | tr 'a-z' 'A-Z'
- 	 "$upercase" > result.log
+ 	 echo "$upercase" > result.log
 done < $1
 echo "file converted to upercase sucessfully"
 } 
