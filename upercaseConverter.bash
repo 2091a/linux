a4aa07f787257092299ec4c2e95313f1b3489962
2091a <anshulbhattglt@gmail.com>
2025-01-03 15:03:04 +0000

    some fix

--- upercaseConverter.bash
+++ upercaseConverter.bash
@@ -5,12 +5,13 @@ cat "$1"
 }
 
 converter(){
+	echo " " > result.log
 	display "$1"
 while read -r line; do
 	echo "converting file to uper case"
 
-	 upercase= "$line" | tr 'a-z' 'A-Z'
- 	 echo "$upercase" > result.log
+	upercase=$(echo "$line" | tr 'a-z' 'A-Z')
+		 echo "$upercase" >> result.log
 done < $1
 echo "file converted to upercase sucessfully"
 } 
